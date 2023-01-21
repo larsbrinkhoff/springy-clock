8703b29a0758c17ed4ef642d91ba5a128d458313
Lars Brinkhoff <lars@nocrew.org>
2023-01-21 15:26:26 +0100

    tmp

--- clock.js
+++ clock.js
@@ -1,9 +1,10 @@
 var canvas;
 var context;
+var milliseconds = 5;
 
 var hands = [
     { radius: .90, width: .01, tension: 1.5, dampening: 0.8 },
-    { radius: .95, width: .03, tension: 0.5, dampening: 0.98 },
+    { radius: .80, width: .03, tension: 0.5, dampening: 0.98 },
     { radius: .60, width: .08, tension: 0.2, dampening: 0.998 }
 ];
 
@@ -23,20 +24,33 @@ function drawHand(hand) {
 }
 
 function drawWindow(x) {
-    context.fillStyle = "rgba(255,255,255,0.6)";
+    context.fillStyle = "rgba(255,255,255,0.3)";
     context.fillRect(0, 0, canvas.width, canvas.height);
+    let size = .9 * Math.min(canvas.width, canvas.height) / 2;
+    context.font = "50px Arial";
+    context.fillStyle = "rgba(0,0,0,1.0)";
+    context.fillText("halv tolv", 20, 100);
+    context.fillText("11:30", 20, 150);
+    for (i = 1; i <= 12; i++) {
+	let a = 3.14159 * ((i-3)/6)
+	context.fillText("" + i,
+                         canvas.width/2 + size * Math.cos(a),
+                         canvas.height/2 + size * Math.sin(a) + 15);
+    }
 }
 
 function updateHand(hand) {
     let force = modulo2PI(hand.target - hand.angle);
-    hand.velocity += hand.tension * force;
-    hand.velocity *= hand.dampening;
-    hand.angle += 0.7 * hand.velocity;
+    hand.velocity += hand.tension * force * milliseconds/30;
+    //hand.velocity *= Math.pow(hand.dampening, milliseconds/30);
+    hand.velocity *= Math.pow(hand.dampening, milliseconds/30);
+    //hand.angle += milliseconds*0.7/30 * hand.velocity;
+    hand.angle += milliseconds*0.7/30 * hand.velocity;
 }
 
 function update() {
     drawWindow();
-    for (i = 0; i < hands.length; i++) {
+    for (i = 1; i < hands.length; i++) {
         updateHand(hands[i]);
         drawHand(hands[i]);
     }
@@ -77,6 +91,6 @@ window.onload = function() {
     hands[1].angle = hands[1].target;
     hands[2].angle = hands[2].target;
 
-    setInterval(update, 30);
+    setInterval(update, milliseconds);
     setInterval(tick, 1000);
 }
